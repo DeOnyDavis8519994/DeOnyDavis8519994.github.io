4b525e1c91b5e8e3d35664d623b6554649038155
DeOnyDavis8519994 <dld8519994@lpssonline.com>
2025-10-09 13:01:24 +0000

    portfolio

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,43 +1,53 @@
- <!DOCTYPE html>
-<html>
-  <head>
-     <title>______________ Computer Science Website</title>
-     <link rel="stylesheet" href="style.css" />
-  </head>
+ $(function () {  // initialize canvas and context when able to
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  window.addEventListener("load", loadJson);
+
+  function setup() {
+    if (firstTimeSetup) {
+      halleImage = document.getElementById("player");
+      projectileImage = document.getElementById("projectile");
+      cannonImage = document.getElementById("cannon");
+      $(document).on("keydown", handleKeyDown);
+      $(document).on("keyup", handleKeyUp);
+      firstTimeSetup = false;
+      //start game
+      setInterval(main, 1000 / frameRate);
+    }
+
+    // Create walls - do not delete or modify this code
+    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
+    createPlatform(-50, -50, 50, canvas.height + 500); // left wall
+    createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
+
+    //////////////////////////////////
+    // ONLY CHANGE BELOW THIS POINT //
+    //////////////////////////////////
+
+    // TODO 1 - Enable the Grid
+     //toggleGrid();
 
 
-  <body>
-    <!-- All content goes here -->
-    <div id="all-contents">
-       <nav>
-        <h1>________________ Amazing Website</h1>
-          <ul id="nav-ul">
-            <li class="nav-li">
-  <a href="index.html">Home</a>
-</li>
-<li class="nav-li">
-  <a href="portfolio.html">Portfolio</a>
-</li>
-</ul>
-  </nav>
+    // TODO 2 - Create Platforms
+    
 
 
-  <main>
-  <div class="content">
-    <h1>Portfolio</h1>
-    <ul id="portfolio">
-<li>
-  <a href="fsd-projects/platformer/">Platformer: A cannon-dodging adventure game for Halleb0t</a>
-</li>
-<li>
-  <a href="fsd-projects/bouncing-box/">Bouncing Box: A fun introduction to web game development</a>
-</li>
-<li>
-  <a href="fsd-projects/circularity/">Circularity: A poetic motion experiment with circles</a>
-</li>
-    </ul>
-  </div>
-  </main>
-    </div>
-  </body>
-</html>
+
+    // TODO 3 - Create Collectables
+
+
+
+   
+    // TODO 4 - Create Cannons
+
+
+   
+   
+    //////////////////////////////////
+    // ONLY CHANGE ABOVE THIS POINT //
+    //////////////////////////////////
+  }
+
+  registerSetup(setup);
+});
